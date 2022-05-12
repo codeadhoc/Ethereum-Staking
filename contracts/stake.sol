b49dd7dccc488c6d747d0f3395d9f539dc7aca70
unknown <viral.v21@gmail.com>
2022-05-12 23:15:24 +0400

    inital repo contracts

---
+++ contracts/stake.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Stakeable {
+    uint256 private totalStaked;
+    uint256 private sumOfRewardPercentOverTime;
+
+    struct Stake {
+        uint256 amount;
+        uint256 since;
+        uint256 sumOfRewardPercentOverTimeAtSince;
+    }
+
+    mapping (address => Stake) public stakes;
+
+    function _stake(uint256 amount) internal {
+        require(stakes[msg.sender].amount == 0, "you have already staked");
+        stakes[msg.sender] = Stake(amount, block.timestamp, sumOfRewardPercentOverTime);
+        totalStaked = totalStaked + amount;
+    }
+
+    function _distribute(uint256 amount) internal {
+        require(totalStaked > 0, "there is no staked tokens");
+
+        /**
+        * Finding what percent the reward is out of total staked. 
+        * As it cannot store decimals therefore first multiply then divide
+        */
+        sumOfRewardPercentOverTime = sumOfRewardPercentOverTime + (((amount*10000/totalStaked) * (100))/100);
+    }
+
+    function calculateReward(address user) public view returns (uint256) {
+        Stake storage stake = stakes[user];
+
+        /**
+        * Calculate the total tokens reward for a user
+        */
+        return ((sumOfRewardPercentOverTime - stake.sumOfRewardPercentOverTimeAtSince)*stake.amount)/10000;   
+    }
+
+    function _unstake(address user) internal returns(uint256) {
+        uint256 amountStaked = stakes[user].amount;
+        require(amountStaked > 0, "user hasn't staked");
+
+        uint256 reward = calculateReward(user);
+        totalStaked = totalStaked - amountStaked;
+        stakes[user].amount = 0;
+
+        return amountStaked + reward;
+    }
+}
